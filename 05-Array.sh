842c9de6893563f4d46861a869c1af05c1345bfc
Kusuma kumari Bondili <bondilikusumakumari@gmail.com>
2025-01-07 11:44:43 +0530

    About Arrays

---
+++ 05-Array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "rrr" "devara")
+
+echo "First Movie is ${MOVIES[0]}"
+echo "Second Movie is ${MOVIES[1]}"
+echo "Third Movie is ${MOVIES[2]}"
+echo "All Movie is ${MOVIES[@]}"
