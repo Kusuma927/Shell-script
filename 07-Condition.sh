2edbfa6900dbb5bef10b8d4c3576a2441ee4fa52
Kusuma kumari Bondili <bondilikusumakumari@gmail.com>
2025-01-08 12:21:28 +0530

    Conidtionalchecking

--- 07-Condition.sh
+++ 07-Condition.sh
@@ -4,6 +4,6 @@ NUMBER=$1
 
 if [ $NUMNER -gt 100 ]
 then
- echo "Given number is greater than $NUMBER
+ echo "Given number is greater than $NUMBER"
 else
- echo "Given number is less than $NUMBER
+ echo "Given number is less than $NUMBER"
